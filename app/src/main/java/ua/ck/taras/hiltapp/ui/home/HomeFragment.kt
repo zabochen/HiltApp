6299855c554d0f345ae005edee5ce972d4af689a
zabochen <zabochen@gmail.com>
2020-06-11 15:42:51 +0300

    Injection: HomeFragment & HomeViewModel by viewModels/activityViewModels.

--- app/src/main/java/ua/ck/taras/hiltapp/ui/home/HomeFragment.kt
+++ app/src/main/java/ua/ck/taras/hiltapp/ui/home/HomeFragment.kt
@@ -1,31 +1,49 @@
 package ua.ck.taras.hiltapp.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import dagger.hilt.android.AndroidEntryPoint
 import ua.ck.taras.hiltapp.R
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val homeViewModel: HomeViewModel by activityViewModels()
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Check viewModel injection
+        with(homeViewModel.stateSaveUi ?: "Null UI State") {
+            Log.i("HomeFragment", "onViewCreated => UIState: $this")
+        }
+        Log.i("HomeFragment", "onViewCreated: ${homeViewModel.getDataList().size}")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        // Save UI State
+        homeViewModel.stateSaveUi = "Saved UI State"
     }
 }
--- app/src/main/java/ua/ck/taras/hiltapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/ua/ck/taras/hiltapp/ui/home/HomeViewModel.kt
@@ -1,13 +1,21 @@
 package ua.ck.taras.hiltapp.ui.home
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ua.ck.taras.hiltapp.data.DataService
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel @ViewModelInject constructor(
+    private val dataService: DataService
+) : ViewModel() {
+
+    var stateSaveUi: String? = null
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    fun getDataList() = dataService.getDataList()
 }
